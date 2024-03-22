30a3788ffb36409b5185fb5eb5c4f2e8e1e05cbb
Nvntdad <nvntdadhich@gmail.com>
2024-03-23 02:49:04 +0530

    Initial commit

---
+++ react-first-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Products from './Products';
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-gray-500 text-center">
+      <h1 className="text-neutral-50">hy nav</h1>
+     
+      <div>
+        <Products />
+      </div>
+    </div>
+    
+  );
+}
+
+export default App
